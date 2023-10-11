13cfe49423a8af481dbc914e2761e1864fe46313
bnares <74542449+bnares@users.noreply.github.com>
2023-10-11 17:33:57 +0200

    Update firebase.js
    hide keys

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getStorage, ref } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZGFHNlED-xqss15P9WG6SZm6RF_uG8cE",
-  authDomain: "chat-b128c.firebaseapp.com",
-  projectId: "chat-b128c",
-  storageBucket: "chat-b128c.appspot.com",
-  messagingSenderId: "373416932756",
-  appId: "1:373416932756:web:d1eca04789783fe9228b81"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
